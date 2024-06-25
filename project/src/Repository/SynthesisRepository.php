88da6eaab48a0090762fa12397fbd656a2caa737
Archaros1 <arthurjung2@hotmail.fr>
2024-06-25 14:16:53 +0200

    controller + better templates

--- project/src/Repository/SynthesisRepository.php
+++ project/src/Repository/SynthesisRepository.php
@@ -5,39 +5,67 @@ namespace App\Repository;
 use App\Entity\Synthesis;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\SecurityBundle\Security;
 
 /**
  * @extends ServiceEntityRepository<Synthesis>
  */
 class SynthesisRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Synthesis::class);
+
+  private $security;
+
+  public function __construct(ManagerRegistry $registry, Security $security)
+  {
+    parent::__construct($registry, Synthesis::class);
+    $this->security = $security;
+  }
+
+  //    /**
+  //     * @return Synthesis[] Returns an array of Synthesis objects
+  //     */
+  //    public function findByExampleField($value): array
+  //    {
+  //        return $this->createQueryBuilder('s')
+  //            ->andWhere('s.exampleField = :val')
+  //            ->setParameter('val', $value)
+  //            ->orderBy('s.id', 'ASC')
+  //            ->setMaxResults(10)
+  //            ->getQuery()
+  //            ->getResult()
+  //        ;
+  //    }
+
+  //    public function findOneBySomeField($value): ?Synthesis
+  //    {
+  //        return $this->createQueryBuilder('s')
+  //            ->andWhere('s.exampleField = :val')
+  //            ->setParameter('val', $value)
+  //            ->getQuery()
+  //            ->getOneOrNullResult()
+  //        ;
+  //    }
+
+  /**
+   * @return Synthesis[] Returns an array of Synthesis objects
+   */
+  public function findByUser(): array
+  {
+    /** @var \App\Entity\Company|null $company */
+    $company = $this->security->getUser();
+
+    if (empty($company)) {
+      // To test without being logged in.
+      $id = 1;
+    }
+    else {
+      $id = $company->getId();
     }
 
-//    /**
-//     * @return Synthesis[] Returns an array of Synthesis objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('s.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Synthesis
-//    {
-//        return $this->createQueryBuilder('s')
-//            ->andWhere('s.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    return $this->createQueryBuilder('s')
+      ->andWhere('s.company = :company_id')
+      ->setParameter('company_id', $id)
+      ->getQuery()
+      ->getResult();
+  }
 }
