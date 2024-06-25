7a3172549dc343cac2b542ecd371443afede6da2
Archaros1 <arthurjung2@hotmail.fr>
2024-06-25 16:38:36 +0200

    fix entrypoint

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -10,9 +10,9 @@ composer install
 # Check if the database exists, and if not, create it and run migrations
 if ! php bin/console doctrine:database:exists --if-not-exists; then
     php bin/console doctrine:database:create
-    php bin/console make:migrations
-    php bin/console doctrine:migrations:migrate --no-interaction
-    # php bin/console doctrine:fixtures:load
+    # php bin/console make:migrations
+    php bin/console doctrine:migrations:migrate
+    php bin/console doctrine:fixtures:load --no-interaction
 fi
 
 # Execute the passed command
