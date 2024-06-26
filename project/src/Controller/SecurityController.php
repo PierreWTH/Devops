a6597c694058eac94dd424acbbe4dbb62990c27f
Archaros1 <arthurjung2@hotmail.fr>
2024-06-26 11:31:36 +0200

    indent security stuff

--- project/src/Controller/SecurityController.php
+++ project/src/Controller/SecurityController.php
@@ -9,26 +9,26 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+  #[Route('/login', name: 'app_login')]
+  public function login(AuthenticationUtils $authenticationUtils): Response
+  {
+    if ($this->getUser()) {
+      return $this->redirectToRoute('app_home');
     }
 
-    /**
-     * @Route("/logout", name="app_logout")
-     */
-    public function logout()
-    {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
-    }
+    // get the login error if there is one
+    $error = $authenticationUtils->getLastAuthenticationError();
+    // last username entered by the user
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+  }
+
+  /**
+   * @Route("/logout", name="app_logout")
+   */
+  public function logout()
+  {
+    throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+  }
 }
