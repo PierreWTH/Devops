adbb8af421f1e0bdf0e9a24eb4cd9dd555a3a8b1
Archaros1 <arthurjung2@hotmail.fr>
2024-06-26 15:56:08 +0200

    css

--- project/src/Form/RegistrationFormType.php
+++ project/src/Form/RegistrationFormType.php
@@ -11,47 +11,46 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('email')
-            ->add('name', TextType::class)
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'Vous devez accepter les conditions générales d\'utilisation.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Merci d\'entrer un mot de passe',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('email', EmailType::class, [
+        'attr' => ['class' => 'form-control'],
+        'label_attr' => ['class' => 'form-label'],
+      ])
+      ->add('name', TextType::class, [
+        'attr' => ['class' => 'form-control'],
+        'label_attr' => ['class' => 'form-label'],
+      ])
+      ->add('plainPassword', PasswordType::class, [
+        'mapped' => false,
+        'attr' => [
+          'autocomplete' => 'new-password',
+          'class' => 'form-control'
+        ],
+        'label_attr' => ['class' => 'form-label'],
+        'constraints' => [
+          new NotBlank([
+            'message' => 'Merci d\'entrer un mot de passe',
+          ]),
+          new Length([
+            'min' => 6,
+            'minMessage' => 'Your password should be at least {{ limit }} characters',
+            'max' => 4096,
+          ]),
+        ],
+      ]);
+  }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => Company::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => Company::class,
+    ]);
+  }
 }
