f4c88bee4b1f8d37b489e8d98f5b113b704219c1
Joey <scheidt.joey@gmail.com>
2024-06-27 16:29:46 +0200

    form interfaces and get data from db

--- project/src/Controller/QuestionsController.php
+++ project/src/Controller/QuestionsController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
-use App\Form\SkillType;
+use App\Entity\Category;
+use App\Entity\Question;
+use App\Form\QuestionType;
+use App\Repository\AxisRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,34 +15,108 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class QuestionsController extends AbstractController
 {
+  private $axisRepository;
+  private $categoryRepository;
+  private $questionRepository;
+
+  public function __construct(AxisRepository $axisRepository, CategoryRepository $categoryRepository, QuestionRepository $questionRepository)
+  {
+    $this->axisRepository = $axisRepository;
+    $this->categoryRepository = $categoryRepository;
+    $this->questionRepository = $questionRepository;
+  }
+
   #[Route('/questions/skill', name: 'questions_skill')]
   public function skill(Request $request): Response
   {
-    $form = $this->createForm(SkillType::class);
+    $axis = $this->axisRepository->find(1);
+    $categories = $this->categoryRepository->findByAxis($axis);
+    $questions = [];
+    $idPrefix = "skill";
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Process form data
-            $data = $form->getData();
-            // Save to database or perform other actions
+    foreach ($categories as $category) {
+      $categoryQuestions = $this->questionRepository->findByCategory($category);
+      $questions = array_merge($questions, $categoryQuestions);
+    }
 
-            return $this->redirectToRoute('questions_reactivity'); // Adjust to your next route
-        }
+    $form = $this->createForm(QuestionType::class, null, ['questions' => $questions]);
+
+    $form->handleRequest($request);
+    dd($form->isSubmitted(), $form);
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $data = $form->getData();
+
+      return $this->redirectToRoute('questions_reactivity');
+    }
 
     return $this->render('forms/questions.html.twig', [
       'form' => $form->createView(),
+      'axis' => $axis,
+      'categories' => $categories,
+      'idPrefix' => $idPrefix,
     ]);
   }
 
   #[Route('/questions/reactivity', name: 'questions_reactivity')]
-  public function reactivity(): Response
+  public function reactivity(Request $request): Response
   {
-    return $this->render('forms/questions.html.twig', []);
+    $axis = $this->axisRepository->find(1);
+    $categories = $this->categoryRepository->findByAxis($axis);
+    $questions = [];
+    $idPrefix = "reactivity";
+
+    foreach ($categories as $category) {
+      $categoryQuestions = $this->questionRepository->findByCategory($category);
+      $questions = array_merge($questions, $categoryQuestions);
+    }
+
+    $form = $this->createForm(QuestionType::class, null, ['questions' => $questions]);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $data = $form->getData();
+
+      return $this->redirectToRoute('questions_numeric');
+    }
+
+    return $this->render('forms/questions.html.twig', [
+      'form' => $form->createView(),
+      'axis' => $axis,
+      'categories' => $categories,
+      'idPrefix' => $idPrefix,
+    ]);
   }
 
   #[Route('/questions/numeric', name: 'questions_numeric')]
-  public function numeric(): Response
+  public function numeric(Request $request): Response
   {
-    return $this->render('forms/questions.html.twig', []);
+    $axis = $this->axisRepository->find(1);
+    $categories = $this->categoryRepository->findByAxis($axis);
+    $questions = [];
+    $idPrefix = "numeric";
+
+    foreach ($categories as $category) {
+      $categoryQuestions = $this->questionRepository->findByCategory($category);
+      $questions = array_merge($questions, $categoryQuestions);
+    }
+
+    $form = $this->createForm(QuestionType::class, null, ['questions' => $questions]);
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $data = $form->getData();
+
+      return $this->redirectToRoute('questions_');
+    }
+
+    return $this->render('forms/questions.html.twig', [
+      'form' => $form->createView(),
+      'axis' => $axis,
+      'categories' => $categories,
+      'idPrefix' => $idPrefix,
+    ]);
   }
 }
